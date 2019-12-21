851a65c1ec81d8e8fc153f96711dc5d0767a211d
minspan1999 <minspan@users.noreply.github.com>
2019-12-21 14:12:28 -0500

    dfa

--- reactJS/refscript.jsx
+++ reactJS/refscript.jsx
@@ -1,13 +1,30 @@
 class RefComponent extends React.Component{
 
+    //constructor to show display
+    constructor(props){
+        super(props);
+        this.display = this.display.bind(this);
+    }
+
+    display(){
+        var name = this.inputName.value;
+        document.getElementById('displayName').innerHTML = name;
+    }
+
     render(){
+        
         return(
             <div>
                 <h2>
-                    Name: <input type="text"/>
-                </h2>
-                <button onClick={}>Click</button>
+                    Name: <input type="text" ref={input => this.inputName = input}/>
+                </h2><br/>
+                <button onClick={this.display}>Click</button>
+                <h1>Hi... <span id="displayName"></span></h1>
             </div>
         )
     }
 }
+
+ReactDOM.render(
+    <RefComponent/>, document.getElementById('refContent')
+)
